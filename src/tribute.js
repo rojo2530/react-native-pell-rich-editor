dd30e6c2a732dddabe0f64a21d1962866460a3b4
Jose Cantos <35645580+rojo2530@users.noreply.github.com>
2020-06-09 13:45:40 +0200

    Update tribute.js

--- src/tribute.js
+++ src/tribute.js
@@ -1,3 +1,4 @@
+alert('hi');
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
